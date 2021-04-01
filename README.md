4637d6b1a7cfcbe91688fa0c24096acf9b349f6e
Michael W <w.eso@live.de>
2021-04-01 07:29:37 +0200

    preSoup changes

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # NoFapSearchPage
 A SearchSite Proxy, which is filtered in multiple ways.
+
+A manual list should be provided.
+
+There will be three different profiles.
+
+1. Definatly Porn
+2. Probably Porn
+3. User Generated Erotik
--- httpserver.py
+++ httpserver.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from flask import Flask, render_template, redirect, url_for, request
+from bs4 import BeautifulSoup
+import requests
 
 
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
 @app.route('/', methods=['POST', 'GET'])
 def search():
@@ -14,11 +12,30 @@ def search():
     if request.method == 'POST':
         if request.form['search'] :
             #return redirect("http://www.bing.com/search?q="+request.form['search'])
-            return render_template('search.html',result="http://www.bing.com/search?q="+request.form['search']) 
+            result="http://www.bing.com/search?q="+build_link(request.form['search'])
+            return render_template('search.html',result=result)
         else:
             error = 'Search'
             return 'error boy'+request.form['search'] 
     # the code below is executed if the request method
     # was GET or the credentials were invalid
     return render_template('search.html')    
-    
+   
+def build_link(formString):
+    words = formString.split()
+    search = ""
+    for w in words:
+    	if(search == ""):
+    	    search = w
+    	else:
+    	    search = search + "+" + w
+    return search
+
+def pullHtml():
+    source = requests.get('https://webscraper.droppages.com/').text
+    soup = BeautifulSoup(source, 'lxml')
+    return(soup)
+
+
+def prepare():
+    return
